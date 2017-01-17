032b77692dc3e2417a01e2d5442c790425f362cb
garciaamor <jgarciaamor@danielcastelao.org>
2017-01-17 08:52:13 +0100

    fragment 2 java importado

--- java/com/example/jgarciaamor/a2t1p/Main2ActivityFragment.java
+++ java/com/example/jgarciaamor/a2t1p/Main2ActivityFragment.java
@@ -1,14 +1,16 @@
 package com.example.jgarciaamor.a2t1p;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import static android.app.Activity.RESULT_OK;
 
-/**
- * A placeholder fragment containing a simple view.
- */
+
 public class Main2ActivityFragment extends Fragment {
 
     public Main2ActivityFragment() {
